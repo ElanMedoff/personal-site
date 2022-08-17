385b37a9cbda6fff50ade9be22e12b630975877a
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-08-16 23:11:35 -0400

    redeploying

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,336 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 5181:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ _app)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+;// CONCATENATED MODULE: external "next/head"
+const head_namespaceObject = require("next/head");
+var head_default = /*#__PURE__*/__webpack_require__.n(head_namespaceObject);
+// EXTERNAL MODULE: ./node_modules/next/link.js
+var next_link = __webpack_require__(1664);
+var link_default = /*#__PURE__*/__webpack_require__.n(next_link);
+// EXTERNAL MODULE: external "classnames"
+var external_classnames_ = __webpack_require__(9003);
+var external_classnames_default = /*#__PURE__*/__webpack_require__.n(external_classnames_);
+;// CONCATENATED MODULE: ./components/Header.tsx
+
+
+
+function Header() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("nav", {
+        className: "sticky top-0 z-10 py-3 border-b-2 border-neutral bg-base-100",
+        children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("main", {
+            className: "flex items-center justify-between w-full px-5",
+            children: [
+                /*#__PURE__*/ jsx_runtime_.jsx("h3", {
+                    className: external_classnames_default()("text-lg md:text-2xl", "font-bold text-primary cursor-pointer"),
+                    children: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                        href: "/",
+                        children: "elanmed.dev"
+                    })
+                }),
+                /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                    children: [
+                        /*#__PURE__*/ (0,jsx_runtime_.jsxs)("span", {
+                            className: "hidden mr-4 text-xs sm:inline",
+                            children: [
+                                "check out my blog! ",
+                                /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                                    className: "text-lg text-primary",
+                                    children: "\u2192"
+                                })
+                            ]
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("span", {
+                            className: "cursor-pointer select-none hover:underline hover:text-primary transition",
+                            children: /*#__PURE__*/ jsx_runtime_.jsx((link_default()), {
+                                href: "/blog",
+                                children: "blog"
+                            })
+                        })
+                    ]
+                })
+            ]
+        })
+    });
+};
+
+;// CONCATENATED MODULE: ./components/Footer.tsx
+
+function Footer() {
+    return /*#__PURE__*/ jsx_runtime_.jsx("footer", {
+        className: "flex justify-center py-4 text-xs border-t-base-300 border-t bg-base-100",
+        children: /*#__PURE__*/ jsx_runtime_.jsx("span", {
+            children: "\xa9 elan medoff"
+        })
+    });
+};
+
+;// CONCATENATED MODULE: ./components/Content.tsx
+
+
+function Content({ children  }) {
+    return /*#__PURE__*/ jsx_runtime_.jsx("main", {
+        className: external_classnames_default()("min-h-screen w-full max-w-5xl m-auto py-6 bg-base-100", "px-2 md:px-20"),
+        children: children
+    });
+};
+
+;// CONCATENATED MODULE: ./components/Layout.tsx
+
+
+
+
+
+function Layout({ children  }) {
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: "bg-base-200",
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)((head_default()), {
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("link", {
+                        rel: "shortcut icon",
+                        href: "/favicon/favicon.ico"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("title", {
+                        children: "elanmed.dev"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "description",
+                        content: "Mostly a blog, partly a personal website"
+                    }),
+                    /*#__PURE__*/ jsx_runtime_.jsx("meta", {
+                        name: "google-site-verification",
+                        content: "CE4T4wzf1pNuiL7JwIC9CqNdJyCfsfaNyLjkeFRr9Dc"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Header, {}),
+            /*#__PURE__*/ jsx_runtime_.jsx(Content, {
+                children: children
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx(Footer, {})
+        ]
+    });
+};
+
+;// CONCATENATED MODULE: ./pages/_app.tsx
+
+
+
+function MyApp({ Component , pageProps  }) {
+    return /*#__PURE__*/ jsx_runtime_.jsx("div", {
+        "data-theme": "cyberpunk",
+        children: /*#__PURE__*/ jsx_runtime_.jsx(Layout, {
+            children: /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+                ...pageProps
+            })
+        })
+    });
+}
+/* harmony default export */ const _app = (MyApp);
+
+
+/***/ }),
+
+/***/ 9003:
+/***/ ((module) => {
+
+module.exports = require("classnames");
+
+/***/ }),
+
+/***/ 3280:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/app-router-context.js");
+
+/***/ }),
+
+/***/ 2796:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 4014:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/i18n/normalize-locale-path.js");
+
+/***/ }),
+
+/***/ 8524:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/is-plain-object.js");
+
+/***/ }),
+
+/***/ 8020:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/mitt.js");
+
+/***/ }),
+
+/***/ 4406:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/page-path/denormalize-page-path.js");
+
+/***/ }),
+
+/***/ 4964:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router-context.js");
+
+/***/ }),
+
+/***/ 1751:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/add-path-prefix.js");
+
+/***/ }),
+
+/***/ 299:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 3938:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/format-url.js");
+
+/***/ }),
+
+/***/ 9565:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-asset-path-from-route.js");
+
+/***/ }),
+
+/***/ 5789:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/get-next-pathname-info.js");
+
+/***/ }),
+
+/***/ 1428:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/is-dynamic.js");
+
+/***/ }),
+
+/***/ 8854:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-path.js");
+
+/***/ }),
+
+/***/ 1292:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/parse-relative-url.js");
+
+/***/ }),
+
+/***/ 4567:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/path-has-prefix.js");
+
+/***/ }),
+
+/***/ 979:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/querystring.js");
+
+/***/ }),
+
+/***/ 3297:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/remove-trailing-slash.js");
+
+/***/ }),
+
+/***/ 6052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/resolve-rewrites.js");
+
+/***/ }),
+
+/***/ 4226:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-matcher.js");
+
+/***/ }),
+
+/***/ 5052:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/router/utils/route-regex.js");
+
+/***/ }),
+
+/***/ 9232:
+/***/ ((module) => {
+
+module.exports = require("next/dist/shared/lib/utils.js");
+
+/***/ }),
+
+/***/ 6689:
+/***/ ((module) => {
+
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [952,664], () => (__webpack_exec__(5181)));
+module.exports = __webpack_exports__;
+
+})();
