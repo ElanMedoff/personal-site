4ad4f7ed76b16e24c13d004f083e461ce0321dd1
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-08-19 01:15:29 -0400

    remove swc

--- next.config.js
+++ next.config.js
@@ -1,17 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ["localhost", "elanmed.dev"],
   },
   productionBrowserSourceMaps: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /* eslint: { */
+  /*   ignoreDuringBuilds: true, */
+  /* }, */
+  /* typescript: { */
+  /*   ignoreBuildErrors: true, */
+  /* }, */
 };
 
 module.exports = nextConfig;
