d8ea3ab6debb80afc6aad14284dcb55fb4a7de2d
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-08-19 00:21:28 -0400

    redeploying

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "next",
-      script: "npm",
+      script: "/home/elan/.nvm/versions/node/v16.16.0/bin/pm2",
       args: "run prod",
     },
   ],
