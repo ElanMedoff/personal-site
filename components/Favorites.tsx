9ca3e555a40dfd57fe1d66d972eab92a55c3b946
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-09-30 00:17:34 -0400

    deploying

--- components/Favorites.tsx
+++ components/Favorites.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { twMerge as tm } from "tailwind-merge";
 import Banner from "./Banner";
 import SwiperCards from "./SwiperCards";
@@ -6,29 +7,38 @@ function SwiperWrapper({
   paths,
   dir,
   title,
+  children,
 }: {
   paths: string[];
   dir: string;
   title: string;
+  children?: ReactNode;
 }) {
   return (
-    <article className="border-base-300 border-2 pr-2 py-4 rounded-lg">
-      <p
-        className={tm(
-          "uppercase font-bold text-xl mb-3 border-b-4 border-primary w-max ml-7"
-        )}
-      >
-        {title}
-      </p>
-      <SwiperCards
-        slides={paths.map((path, index) => (
-          <div key={index} className="p-1 rounded bg-neutral">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={`/${dir}/${path}`} alt={dir} className="" />
-          </div>
-        ))}
-        className="max-w-[150px] [@media(min-width:375px)]:max-w-[200px] mx-7"
-      />
+    <article
+      className={tm("border-base-300 border-2 pr-2 py-4 rounded-lg", "flex")}
+    >
+      <section>
+        <SwiperCards
+          slides={paths.map((path, index) => (
+            <div key={index} className="p-1 rounded bg-neutral">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={`/${dir}/${path}`} alt={dir} className="" />
+            </div>
+          ))}
+          className="max-w-[150px] [@media(min-width:375px)]:max-w-[200px] mx-7"
+        />
+      </section>
+      <section>
+        <h3
+          className={tm(
+            "uppercase font-bold text-xl mb-3 border-b-4 border-primary w-max ml-7"
+          )}
+        >
+          {title}
+        </h3>
+        <p className={tm("max-w-xs")}>{children}</p>
+      </section>
     </article>
   );
 }
@@ -48,7 +58,9 @@ export default function Favorites({
       <section
         className={tm("flex justify-center flex-wrap items-center gap-8")}
       >
-        <SwiperWrapper paths={moviePaths} dir="movies" title="movies" />
+        <SwiperWrapper paths={moviePaths} dir="movies" title="movies">
+          this is a description, some text that will go here when I decide
+        </SwiperWrapper>
         <SwiperWrapper paths={bookPaths} dir="books" title="books" />
         <SwiperWrapper paths={comicPaths} dir="comics" title="comics" />
       </section>
