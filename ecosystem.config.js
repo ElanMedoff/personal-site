35aa1be3efd07690f4538d718107af727aeb3aa0
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-11-02 21:46:44 -0400

    remove build from pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   apps: [
     {
-      name: "build",
-      script: "/home/elan/.nvm/versions/node/v16.16.0/bin/npm",
-      args: "run build",
-    },
-    {
       name: "start",
       script: "/home/elan/.nvm/versions/node/v16.16.0/bin/npm",
       args: "run start",
