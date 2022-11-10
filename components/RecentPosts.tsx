2bccce9f1e25745494f345d7e8f154892dbe7362
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-11-09 20:32:58 -0500

    update height on small screens

--- components/RecentPosts.tsx
+++ components/RecentPosts.tsx
@@ -10,7 +10,7 @@ function Post({ post }: { post: Metadata }) {
     <Link href={`/blog/${post.slug}`}>
       <div
         className={tm(
-          "cursor-pointer rounded-2xl px-9 py-6 bg-base-100 h-60",
+          "cursor-pointer rounded-2xl px-9 py-6 bg-base-100 h-72 sm:h-60",
           "flex flex-col justify-between",
           "shadow-lg hover:shadow-2xl",
           "border hover:border-primary",
