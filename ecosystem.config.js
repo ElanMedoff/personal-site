be95b9e41708a5ac40e046298c47240d37538d4b
Elan <54599408+ElanMedoff@users.noreply.github.com>
2023-04-12 11:19:41 -0400

    update post receive hook

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: "start",
-      script: "/home/elan/.nvm/versions/node/v16.16.0/bin/npm",
-      args: "run start",
-    },
-  ],
-};
--- post-receive.sh
+++ post-receive.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-LOG=/home/elan/post-receive.log
+LOG=/home/elan/elanmed.dev.log
+ECO=/home/elan/ecosystem.config.js
 NPM=/home/elan/.nvm/versions/node/v16.16.0/bin/npm
 NPX=/home/elan/.nvm/versions/node/v16.16.0/bin/npx
 PM2=/home/elan/.nvm/versions/node/v16.16.0/bin/pm2
@@ -22,6 +23,6 @@ $NPX prisma migrate deploy >> $LOG 2>&1
 echo "migrated prisma" > $LOG
 
 echo "restarting pm2 daemon..." >> $LOG
-$PM2 reload ecosystem.config.js --only start >> $LOG 2>&1
+$PM2 reload $ECO --only elanmed >> $LOG 2>&1
 echo "restarted pm2 daemon" >> $LOG
 
