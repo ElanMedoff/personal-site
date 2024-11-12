e3fb598687766820c6454fe4f737730cbfe52b46
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-11-11 22:35:33 -0500

    update ps.sh to use pwd, no longer require an arg

--- ps.sh
+++ ps.sh
@@ -6,24 +6,26 @@ then
   exit
 fi
 
+current_dir=$(pwd)
+
 uuid=$(uuidgen)
 uuid=${uuid:0:3}
 tmux new-session -d -s "n-${uuid}"
 window=0
 tmux rename-window -t "n-${uuid}":0 "code"
-tmux send-keys "nvim " "$1" C-m
+tmux send-keys "nvim " "$current_dir" C-m
 tmux split-window -h
-tmux send-keys "builtin cd $1 && clear" "C-m"
+tmux send-keys "builtin cd $current_dir && clear" "C-m"
 tmux select-pane -L
 tmux resize-pane -Z
 window=1
 tmux new-window -t "n-${uuid}":1
 tmux select-window -t "n-${uuid}":1
 tmux rename-window -t "n-${uuid}":1 "server"
-tmux send-keys "builtin cd $1 && clear" C-m
+tmux send-keys "builtin cd $current_dir && clear" C-m
 tmux send-keys "open http://localhost:3001 && npm run dev" C-m
 tmux split-window -h
-tmux send-keys "builtin cd $1 && clear" C-m
+tmux send-keys "builtin cd $current_dir && clear" C-m
 tmux send-keys "npx prisma studio" C-m
 tmux select-window -t "n-${uuid}":0
 tmux attach-session -t "n-${uuid}"
