3127213abad366baa08706e4a0f30b85e08d6fba
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-05-23 23:07:11 -0400

    add more descriptive options to t.sh

--- bash-scripts/t.sh
+++ bash-scripts/t.sh
@@ -9,13 +9,13 @@ while getopts "t:a:" opt; do
   case $opt in
     t)
       case $OPTARG in
-        u) ;&
-        vl) ;&
-        vr)
+        unit) ;&
+        validate-links) ;&
+        visual-regression)
           TYPE=$OPTARG
           ;;
         *)
-          cecho --mode=error "-t {u,vr,vl}"
+          cecho --mode=error "-t(ype) {unit,visual-regression,validate-links}"
           exit 1
           ;;
       esac
@@ -27,13 +27,13 @@ while getopts "t:a:" opt; do
           ACTION=$OPTARG
           ;;
         *)
-          cecho --mode=error "-a {update,compare}"
+          cecho --mode=error "-a(ction) {update,compare}"
           exit 1
           ;;
       esac
       ;;
     ?)
-      cecho --mode=error "-t {u,vr} -a {update,compare}"
+      cecho --mode=error "-t(ype) {unit,visual-regression,validate-links} -a(ction) {update,compare}"
       exit 1
       ;;
   esac
@@ -42,22 +42,22 @@ done
 shift "$((OPTIND - 1))"
 
 if [[ $TYPE == "" ]]; then
-  cecho --mode=error 'the `t` flag is required'
+  cecho --mode=error 'the `t(ype)` flag is required'
   exit 1
 fi
 
-if [[ $TYPE == "u" ]] && [[ $ACTION != "" ]]; then
-  cecho --mode=error '`u` cannot be combined with any other flags'
+if [[ $TYPE == "unit" ]] && [[ $ACTION != "" ]]; then
+  cecho --mode=error '`unit` cannot be combined with any other flags'
   exit 1
 fi
 
-if [[ $TYPE == "vl" ]] && [[ $ACTION != "" ]]; then
-  cecho --mode=error '`vl` cannot be combined with any other flags'
+if [[ $TYPE == "validate-links" ]] && [[ $ACTION != "" ]]; then
+  cecho --mode=error '`validate-links` cannot be combined with any other flags'
   exit 1
 fi
 
-if [[ $TYPE == "vr" ]] && [[ $ACTION == "" ]]; then
-  cecho --mode=error 'the `t` flag must be combined with an `a` flag'
+if [[ $TYPE == "visual-regression" ]] && [[ $ACTION == "" ]]; then
+  cecho --mode=error 'the `t(ype)` flag must be combined with an `a(ction)` flag'
   exit 1
 fi
 
@@ -73,19 +73,19 @@ pm2 start "npx prisma studio --browser none" --name "$PM2_PRISMA_NAME"
 PM2_NEXT_NAME=""
 CMD=""
 
-if [[ $TYPE == "u" ]]; then
+if [[ $TYPE == "unit" ]]; then
   PM2_NEXT_NAME="unit-test-suite"
   CMD="npm run unit $1"
   pm2 start "npm run dev" --name "$PM2_NEXT_NAME"
 fi
 
-if [[ $TYPE == "vl" ]]; then
+if [[ $TYPE == "validate-links" ]]; then
   PM2_NEXT_NAME="validate-links-test-suite"
   CMD="npm run validate-links"
   pm2 start "npm run dev" --name "$PM2_NEXT_NAME"
 fi
 
-if [[ $TYPE == "vr" ]]; then
+if [[ $TYPE == "visual-regression" ]]; then
   PM2_NEXT_NAME="visual-regression-test-suite"
   pm2 start "npm run dev:visual-regression" --name "$PM2_NEXT_NAME"
 
